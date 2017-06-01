45b43fe2dc927ea5d120ffbd4e10b792f18a83de
kimikomu <kimikomu@gmail.com>
2017-06-01 16:19:55 -0700

    Updated Comic Book Detail view to display comic book information.

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -6,10 +6,13 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
+	// create a controller from the controller parent
 	public class ComicBookController : Controller
 	{
+		// call an action method that returns an ActionResult type
 		public ActionResult Detail()
 		{
+			// Displays the file named after this method (Detail) from the folder named after the controller calling the method (ComicBook) in the Views folder.
 			return View();
 		}
 	}
