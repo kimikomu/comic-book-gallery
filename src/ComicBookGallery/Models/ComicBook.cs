cffdb82f7a96bd929d003a00b7f5d26461f21305
kimikomu <kimikomu@gmail.com>
2017-06-02 16:08:52 -0700

    Updated comic book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -13,5 +13,23 @@ namespace ComicBookGallery.Models
 		public string DescriptionHtml { get; set; }	// adding Html to the name makes it clear that it will have html content
 		public Artists[] Artists { get; set; }	// array of objects of type Artists
 		public bool Favorite { get; set; }	// indicates whether or not this comic is a favorite of ours or not
+
+		public string DisplayText	// uniquely identifies the comic book for our users. readonly by not defining a setter
+		{
+			get
+			{
+				return SeriesTitle + " #" + IssueNumber;
+			}
+		}
+
+		// series-title-issuenumber.jpg
+		public string CoverImageFileName
+		{
+			get
+			{
+				return SeriesTitle.Replace(" ", "-").	// string.Replace method replaces first parameter with second
+					ToLower() + "-" + IssueNumber + ".jpg";
+			}
+		}
 	}
 }
