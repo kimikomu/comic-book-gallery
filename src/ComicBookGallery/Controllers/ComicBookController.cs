30f279767fb97f34f9a1623ddf8912d8b610525c
kimikomu <kimikomu@gmail.com>
2017-06-02 16:51:06 -0700

    Updated Comic Book Detail view to be strongly typed

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Models;
 
 namespace ComicBookGallery.Controllers
 {
@@ -12,20 +13,23 @@ namespace ComicBookGallery.Controllers
 		// call an action method that returns an ActionResult type
 		public ActionResult Detail()
 		{
-			ViewBag.SeriesTitle = "The Amazing Spider-Man";
-			ViewBag.IssueNumber = 700;
-			ViewBag.Description = "<p>Final issue! Witness the last hours of Doc Octopus' life and his one last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-			ViewBag.Artists = new string[]
+			var comicBook = new ComicBook()
 			{
-				"Script: Dan Slott",
-				"Pencils: Humberto Ramos",
-				"Inks: Victor Olazaba",
-				"Colors: Edgar Delgado",
-				"Letters: Chris Eliopoulos"
-			};
+				SeriesTitle = "The Amazing Spider-Man",
+				IssueNumber = 700,
+				DescriptionHtml = "<p>Final issue! Witness the last hours of Doc Octopus' life and his one last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+				Artists = new Artists[]
+				{
+					new Artists() { Name = "Dan Slott", Role = "Script" },
+					new Artists() { Name = "Humberto Ramos", Role = "Pencils" },
+					new Artists() { Name = "Victor Olazaba", Role = "Inks" },
+					new Artists() { Name = "Edgar Delgado", Role = "Colors" },
+					new Artists() { Name = "Chris Eliopoulos", Role = "Letters" }
+				}
+		};
 
 			// Displays the file named after this method (Detail) from the folder named after the controller calling the method (ComicBook) in the Views folder.
-			return View();
+			return View(comicBook);
 		}
 	}
 }
