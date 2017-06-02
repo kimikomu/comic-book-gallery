0c42f5029534c15480ed91fc8ea0f976e26dc20f
kimikomu <kimikomu@gmail.com>
2017-06-02 15:54:34 -0700

    Added comic book and artists models

---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+	public class ComicBook
+	{
+		public int Id { get; set; }	// uniquely identifies a comic book
+		public string SeriesTitle { get; set; }	// represents the title of the series this comic book is part of
+		public int IssueNumber { get; set; }	// comic book's issue number
+		public string DescriptionHtml { get; set; }	// adding Html to the name makes it clear that it will have html content
+		public Artists[] Artists { get; set; }	// array of objects of type Artists
+		public bool Favorite { get; set; }	// indicates whether or not this comic is a favorite of ours or not
+	}
+}
