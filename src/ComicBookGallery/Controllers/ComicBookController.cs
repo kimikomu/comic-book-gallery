214fb272cf43d5eaeca0afa9705d514b314d8274
kimikomu <kimikomu@gmail.com>
2017-06-01 21:27:57 -0700

    Updated Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -12,6 +12,18 @@ namespace ComicBookGallery.Controllers
 		// call an action method that returns an ActionResult type
 		public ActionResult Detail()
 		{
+			ViewBag.SeriesTitle = "The Amazing Spider-Man";
+			ViewBag.IssueNumber = 700;
+			ViewBag.Description = "<p>Final issue! Witness the last hours of Doc Octopus' life and his one last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+			ViewBag.Artists = new string[]
+			{
+				"Script: Dan Slott",
+				"Pencils: Humberto Ramos",
+				"Inks: Victor Olazaba",
+				"Colors: Edgar Delgado",
+				"Letters: Chris Eliopoulos"
+			};
+
 			// Displays the file named after this method (Detail) from the folder named after the controller calling the method (ComicBook) in the Views folder.
 			return View();
 		}
